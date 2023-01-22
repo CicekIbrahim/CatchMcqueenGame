d9ca2bb570bf0b42fcc38747a5ca21043e15a679
Ibrahim Cicek <cicekibrahim35@icloud.com>
2023-01-22 09:27:43 +0300

    Initial Commit

---
+++ CatchMcQueen/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CatchMcQueen
+//
+//  Created by Ibrahim Cicek on 22.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ CatchMcQueenUITests/CatchMcQueenUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  CatchMcQueenUITestsLaunchTests.swift
+//  CatchMcQueenUITests
+//
+//  Created by Ibrahim Cicek on 22.01.2023.
+//
+
+import XCTest
+
+final class CatchMcQueenUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
